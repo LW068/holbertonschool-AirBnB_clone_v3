e1cb8cbdc37d13eff58bfa578e6009c99d61b9d1
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-04-05 20:57:18 -0700

    fixing pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -35,4 +35,3 @@ if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = os.environ.get("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
-
